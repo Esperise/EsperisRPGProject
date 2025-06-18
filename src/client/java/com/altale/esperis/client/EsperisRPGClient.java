54ddc56c5cd0c32455ad231665a33c7d9439f7ab
Esperise <esperis@naver.com>
2025-06-18 22:48:08 +0900

    health hud test ver1. text

--- src/client/java/com/altale/esperis/client/EsperisRPGClient.java
+++ src/client/java/com/altale/esperis/client/EsperisRPGClient.java
@@ -1,10 +1,12 @@
 package com.altale.esperis.client;
-
+import com.altale.esperis.client.healthHUD.HealthHUDOverlay;
 import net.fabricmc.api.ClientModInitializer;
 
 public class EsperisRPGClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        HealthHUDOverlay.register();
+
     }
 }
