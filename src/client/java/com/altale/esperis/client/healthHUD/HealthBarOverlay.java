ce099b309e5b16ceb199af178f483665aef8c1b8
EsperisDesk <esperis@naver.com>
2025-06-20 01:12:08 +0900

    플레이어 체력 바 ver2

--- src/client/java/com/altale/esperis/client/healthHUD/HealthBarOverlay.java
+++ src/client/java/com/altale/esperis/client/healthHUD/HealthBarOverlay.java
@@ -25,7 +25,7 @@ public class HealthBarOverlay {
                 int aborptionBar = (int)((aborption / healthWithAborption) * barWidth);
 
                 String healthText = "";float textX=0;float textY=0;
-                int x = 125;
+                int x = client.getWindow().getScaledWidth()/2 -barWidth -8;
                 int y = client.getWindow().getScaledHeight() - 41;
                 if(aborption > 0) {
                     healthText = String.format("%.0f (+%.0f) / %.0f",cur,aborption,max);
@@ -41,15 +41,15 @@ public class HealthBarOverlay {
 
 
                 // 배경
-                drawContext.fill(x, y, x + barWidth, y + barHeight, 0xFF333333);
+                drawContext.fill(x-1, y-1, x + barWidth+1, y + barHeight+1, 0x55FFFFFF);
 
                 // 체력 바
                 MatrixStack matrices = drawContext.getMatrices();
 
-                drawContext.fill( x - 1, y - 2, x + barWidth + 1, y + barHeight + 2, 0xFF000000);
+                drawContext.fill( x , y , x + barWidth , y + barHeight , 0xFF000000);//빈 체력
 
-            drawContext.fill( x, y, x + filledWidth, y +barHeight, 0xFFFF5555);
-            drawContext.fill( x+filledWidth, y, x + filledWidth+aborptionBar, y +barHeight, 0xFFFFFFFF);
+            drawContext.fill( x, y, x + filledWidth+1, y +barHeight, 0xFFFF3333); //빨간 체력
+            drawContext.fill( x+filledWidth+1, y, x+1 + filledWidth+aborptionBar, y +barHeight, 0xFFFFFFFF);//하얀 체력
             // 3. 텍스트 렌더링 (흰색 + 검정 outline)
             TextRenderer renderer = client.textRenderer;
             OrderedText text = Text.literal(healthText).asOrderedText();
