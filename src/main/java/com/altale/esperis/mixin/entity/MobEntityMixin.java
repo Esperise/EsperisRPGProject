c4a663953e36857df18b304232b0e84276096c80
Esperise <esperis@naver.com>
2025-10-09 11:39:23 +0900

    vanilla 낙하 중 공격시 150% 데미지 입히는 거 없애는 거 테스트 버전

--- src/main/java/com/altale/esperis/mixin/entity/MobEntityMixin.java
+++ src/main/java/com/altale/esperis/mixin/entity/MobEntityMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,6 +26,23 @@ public abstract class MobEntityMixin {
     ) {
         MobEntity self = (MobEntity)(Object)this;
         Random random = new Random();
+        Difficulty currentDifficulty = world.getDifficulty();
+        int hpBoost = 0;
+        int atkBoost= 0;
+        switch(currentDifficulty){
+            case EASY ->{
+                hpBoost = self.getRandom().nextInt(8)+8;
+                atkBoost = self.getRandom().nextInt(5);
+            }
+            case NORMAL ->{
+                hpBoost = self.getRandom().nextInt(10)+10;
+                atkBoost = self.getRandom().nextInt(6)+2;
+            }
+            case HARD ->{
+                hpBoost = self.getRandom().nextInt(15)+20;
+                atkBoost = self.getRandom().nextInt(8)+4;
+            }
+        }
 
         // 체력 속성 가져오기
         EntityAttributeInstance healthAttr = self.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
@@ -35,10 +53,10 @@ public abstract class MobEntityMixin {
             double baseHealth = healthAttr.getBaseValue();
             double multiplier = 1.0 + self.getRandom().nextDouble();
             if(self instanceof HostileEntity){
-                baseHealth+= self.getRandom().nextInt(15 )+20;
+                baseHealth+= hpBoost;
                 if(attackAttr != null){
                     double baseAttack= attackAttr.getBaseValue();
-                    baseAttack += random.nextInt(7)+3;
+                    baseAttack += atkBoost;
                     attackAttr.setBaseValue(baseAttack);
                 }
                 if(speedAttr != null && !self.isBaby()){
